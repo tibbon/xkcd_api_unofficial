65533d6d804abad981828e14593704342999645d
David Fisher <tibbon@gmail.com>
2013-11-25 11:27:05 -0500

    Import all rake task done

---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20131125161745) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "webcomics", force: true do |t|
+    t.integer  "month"
+    t.integer  "num"
+    t.text     "link"
+    t.integer  "year"
+    t.text     "news"
+    t.text     "safe_title"
+    t.text     "transcript"
+    t.text     "title_text"
+    t.text     "alt"
+    t.text     "img"
+    t.text     "title"
+    t.integer  "day"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
--- lib/tasks/xkcd.rake
+++ lib/tasks/xkcd.rake
@@ -1,14 +1,12 @@
 namespace :xkcd do
   desc "Imports all XKCD comics to database"
   task import_all: :environment do
-  	binding.pry
-
   	# Get number of most recent comic:
   	recent_comic = HTTParty.get('http://xkcd.com/info.0.json')
   	last_comic_number = recent_comic["num"]
   	for x in 1..last_comic_number
   		comic = HTTParty.get("http://xkcd.com/#{x}/info.0.json")
-
+  		insert_comic_to_database(comic)
   	end
 
   end
@@ -25,6 +23,17 @@ end
 
 def insert_comic_to_database(comic)
 	Webcomic.create(
-		
+		month: comic["month"],
+		num: comic["num"],
+		link: comic["link"],
+		year: comic["year"],
+		news: comic["news"],
+		safe_title: comic["safe_title"],
+		transcript: comic["transcript"],
+		title_text: comic["title_text"],
+		alt: comic["alt"],
+		img: comic["img"],
+		title: comic["title"],
+		day: comic["day"]
 	)
 end
