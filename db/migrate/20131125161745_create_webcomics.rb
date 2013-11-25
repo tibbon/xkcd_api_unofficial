d8d2eb4e5e32e46496a6b349cf4e15b367a29e47
David Fisher <tibbon@gmail.com>
2013-11-25 11:22:37 -0500

    Updated rake task a bit, updated migration

--- db/migrate/20131125161745_create_webcomics.rb
+++ db/migrate/20131125161745_create_webcomics.rb
@@ -2,16 +2,16 @@ class CreateWebcomics < ActiveRecord::Migration
   def change
     create_table :webcomics do |t|
       t.integer :month
-      t.integer :num
+      t.integer :num, index: true, unique: true
       t.text :link
-      t.integer :year
+      t.integer :year, index: true
       t.text :news
       t.text :safe_title
       t.text :transcript
       t.text :title_text
-      t.text :alt
+      t.text :alt, index: true
       t.text :img
-      t.text :title
+      t.text :title, index: true
       t.integer :day
 
       t.timestamps
--- lib/tasks/xkcd.rake
+++ lib/tasks/xkcd.rake
@@ -2,6 +2,14 @@ namespace :xkcd do
   desc "Imports all XKCD comics to database"
   task import_all: :environment do
   	binding.pry
+
+  	# Get number of most recent comic:
+  	recent_comic = HTTParty.get('http://xkcd.com/info.0.json')
+  	last_comic_number = recent_comic["num"]
+  	for x in 1..last_comic_number
+  		comic = HTTParty.get("http://xkcd.com/#{x}"/info.0.json)
+  	end
+
   end
 
   desc "Imports one XKCD comic to database"
