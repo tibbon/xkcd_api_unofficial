d202daf31fc9017b118217c3328259b8270640e1
David Fisher <tibbon@gmail.com>
2014-02-25 12:41:33 -0500

    Updated with pagination

--- app/controllers/webcomics_controller.rb
+++ app/controllers/webcomics_controller.rb
@@ -2,7 +2,11 @@ class WebcomicsController < ApplicationController
 	def show
 		@webcomic_params = webcomic_params
 		if @api_key == "foobar"
-			@webcomic = Webcomic.where(@webcomic_params)
+			if @webcomic_params[:limit]
+				@webcomic = Webcomic.where(@webcomic_params).limit(@webcomic_params[:limit])
+			else
+				@webcomic = Webcomic.where(@webcomic_params)
+			end
 		else
 			@webcomic = Webcomic.where(@webcomic_params).limit(3)
 		end
